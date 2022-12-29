9f9a064dc0cfc4d8bfd675586be66733383cc0e8
devjoshixo <djoshi923@gmail.com>
2022-12-29 13:26:31 +0530

    Dashboard api done

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const port = process.env.PORT;
 const app = express();
+
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Dashboard");
 });
+
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
