f358e2337cd101d4aae73e183ea9090adb16a702
devjoshixo <djoshi923@gmail.com>
2022-12-29 13:35:39 +0530

    Added contacts api

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const app = express();
-
 
 app.get("/", (req, res) => {
   res.send("Dashboard");
 });
 
+app.get("/contacts", (req,res) => {
+  res.send("Contacts")
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
