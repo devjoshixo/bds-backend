30d8a2bb719b76e41683b7eab82af925edb954f4
Chirag Gupta <91630704+CHIRAG-GUPTA-987@users.noreply.github.com>
2022-12-31 16:54:15 +0530

    ...

--- Routes/user.js
+++ Routes/user.js
@@ -1,16 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../Controller/user");
-const passport = require("passport");
 
 router.route("/register").post(User.createUser);
-router.route("/login").post(
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/",
-  }),
-  User.login
-);
+router.route("/login").post(User.login);
 router.route("/logout").get(User.logoutUser);
 
 module.exports = router;
