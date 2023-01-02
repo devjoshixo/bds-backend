835b74b0a798c211846a060106bc544ff5e92999
Chirag Gupta <91630704+CHIRAG-GUPTA-987@users.noreply.github.com>
2023-01-02 11:46:58 +0530

    ..

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -4,13 +4,17 @@ const jsonSecret = process.env.SECRET;
 module.exports = async (req, res, next) => {
   const token = req.header("auth-token");
   if (!token) {
-    res.status(401).send("Please authenticate to generate a token");
+    res
+      .status(401)
+      .json({ errorMessage: "Please authenticate to generate a token" });
   }
   try {
     const data = jwt.verify(token, jsonSecret);
     req.user = data.user;
     next();
   } catch (error) {
-    res.status(401).send("Please authenticate to generate a token");
+    res
+      .status(401)
+      .json({ errorMessage: "Please authenticate to generate a token" });
   }
 };
